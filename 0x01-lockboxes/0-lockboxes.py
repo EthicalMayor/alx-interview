b9c208272a1ce23297b1c5c969904a5ffd629333
daadesina <daadesina1@gmail.com>
2024-09-17 22:43:25 +0100

    mayowa

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
-""" n number of boxes
-   each box is numbered sequentially from 0 to n - 1 and
-   each box may contain keys to the other boxes."""
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """ method that determines if all the boxes can be opened """
-    unlocked = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
             continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
